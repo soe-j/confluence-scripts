eb05c069cc057f714a5eda5334915826436f8ef0
soe-j <7115203+soe-j@users.noreply.github.com>
2020-06-27 18:41:01 +0900

    request to create page

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const dotenv = require('dotenv');
 const config = dotenv.config().parsed;
 
+const axios = require('axios');
+
 const createPage = () => {
-  console.log(config);
+  axios.request({
+    method: 'post',
+    baseURL: `https://${config.DOMAIN}`,
+    url: '/wiki/rest/api/content',
+    auth: {
+      username: config.USER,
+      password: config.TOKEN
+    },
+    data: {
+      title: "2020-MM-DD 小ページ作った from js",
+      type: "page",
+      space: {
+        key: config.SPACE
+      },
+      ancestors: [{
+        id: config.PAGE
+      }],
+      body: {
+        storage: {
+          value: "<h2>すごいページ</h2>すごいページです。とっても。",
+          representation: "storage"
+        }
+      }
+    }
+  });
 }
 
 exports.createPage = createPage
